91d066fc9e73aed027d7879b7a69f2f4550ff6e4
ShinGyeongju <sinkj98@naver.com>
2023-02-28 17:30:49 +0900

    전국 화장실 데이터 다운로드 Url 추가

--- Common/config.js
+++ Common/config.js
@@ -12,12 +12,6 @@ module.exports.init = () => {
     port: process.env.HTTP_PORT
   }
 
-  module.exports.apiKeyConfig = {
-    kakaoMap: process.env.KAKAKO_MAP_API_KEY,
-    naverMapId: process.env.NAVER_MAP_API_ID,
-    naverMapSecret: process.env.NAVER_MAP_API_SECRET
-  }
-
   module.exports.mongoConfig_Log = {
     address: process.env.MONGO_LOG_ADDRESS,
     port: process.env.MONGO_LOG_PORT,
@@ -40,5 +34,33 @@ module.exports.init = () => {
     user: process.env.POSTGRES_TOILET_USER,
     password: process.env.POSTGRES_TOILET_PASSWORD
   }
+
+  module.exports.apiConfig = {
+    kakaoMapUrl: process.env.KAKAO_MAP_API_URL,
+    kakaoMap: process.env.KAKAKO_MAP_API_KEY,
+    naverMapUrl: process.env.NAVER_MAP_API_URL,
+    naverMapId: process.env.NAVER_MAP_API_ID,
+    naverMapSecret: process.env.NAVER_MAP_API_SECRET
+  }
+
+  module.exports.toiletDownloadConfig = [
+    {region: '서울특별시', url: process.env.TOILET_EXCEL_URL_SEOUL},
+    {region: '부산광역시', url: process.env.TOILET_EXCEL_URL_BUSAN},
+    {region: '대구광역시', url: process.env.TOILET_EXCEL_URL_DAEGU},
+    {region: '인천광역시', url: process.env.TOILET_EXCEL_URL_INCHEON},
+    {region: '광주광역시', url: process.env.TOILET_EXCEL_URL_GWANGJU},
+    {region: '대전광역시', url: process.env.TOILET_EXCEL_URL_DAEJEON},
+    {region: '울관광역시', url: process.env.TOILET_EXCEL_URL_ULSAN},
+    {region: '세종특별자치시', url: process.env.TOILET_EXCEL_URL_SEJONG},
+    {region: '경기도', url: process.env.TOILET_EXCEL_URL_GYEONGGI},
+    {region: '강원도', url: process.env.TOILET_EXCEL_URL_GANGWON},
+    {region: '충청북도', url: process.env.TOILET_EXCEL_URL_CHUNGBUK},
+    {region: '충청남도', url: process.env.TOILET_EXCEL_URL_CHUNGNAM},
+    {region: '전라북도', url: process.env.TOILET_EXCEL_URL_JEONBUK},
+    {region: '전라남도', url: process.env.TOILET_EXCEL_URL_JEONNAM},
+    {region: '경상북도', url: process.env.TOILET_EXCEL_URL_GYEONGBUK},
+    {region: '경상남도', url: process.env.TOILET_EXCEL_URL_GYEONGNAM},
+    {region: '제주특별자치도', url: process.env.TOILET_EXCEL_URL_JEJU}
+  ]
 
 }
