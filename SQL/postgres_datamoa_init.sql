19ebe40e4c7ece03e6737ce24b53f153698c3f39
ShinGyeongju <sinkj98@naver.com>
2023-03-18 01:11:07 +0900

    docker-compose 및 init SQL 수정

--- SQL/postgres_datamoa_init.sql
+++ SQL/postgres_datamoa_init.sql
@@ -1,8 +1,8 @@
 
-CREATE DATABASE datamoa;
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+---------------------------------------------------------------
+-- Datamoa
 ---------------------------------------------------------------
 
 CREATE TABLE tb_page_category (
@@ -53,3 +53,60 @@ INSERT INTO tb_page_category (category_name) VALUES ('위치 찾기');
 
 INSERT INTO tb_page (category_id, page_name, page_url, page_description)
 VALUES (1, '화장실 찾기', '/toilet', '화장실 위치 찾기 페이지입니다.');
+
+
+---------------------------------------------------------------
+-- Toilet
+---------------------------------------------------------------
+
+CREATE DATABASE toilet;
+\connect toilet;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE tb_toilet_category (
+    toilet_category_id INT GENERATED ALWAYS AS IDENTITY,
+    toilet_category_name VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_toilet_category_id PRIMARY KEY (toilet_category_id)
+);
+
+CREATE TABLE tb_toilet (
+    toilet_id UUID DEFAULT uuid_generate_v4(),
+    toilet_category_id INT NOT NULL,
+    toilet_name VARCHAR(60)[] NOT NULL,
+    toilet_region VARCHAR(20) NOT NULL,
+    toilet_address VARCHAR(100),
+    toilet_road_address VARCHAR(100),
+    management_agency VARCHAR(40),
+    phone_number VARCHAR(20),
+    open_hour VARCHAR(60),
+    wsg84_x DOUBLE PRECISION NOT NULL,
+    wsg84_y DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    CONSTRAINT pk_toilet_id PRIMARY KEY (toilet_id),
+    CONSTRAINT fk_toilet_category_id FOREIGN KEY (toilet_category_id)
+        REFERENCES "tb_toilet_category"(toilet_category_id)
+        ON UPDATE CASCADE
+);
+
+---------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION func_update_time()
+    RETURNS TRIGGER AS
+        $$ BEGIN
+            NEW.updated_at = now();
+            RETURN NEW;
+        END; $$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER trig_update_time
+    BEFORE UPDATE ON tb_toilet
+    FOR EACH ROW
+    EXECUTE PROCEDURE func_update_time();
+
+---------------------------------------------------------------
+
+INSERT INTO tb_toilet_category (toilet_category_name)
+    VALUES ('공중 화장실'), ('개방 화장실'), ('간이 화장실'), ('기타');
+
