91b0d73b96b702db21e2ff90c4f4ef619ef0ac11
ShinGyeongju <sinkj98@naver.com>
2023-03-20 23:43:03 +0900

    CSP 관련 설정 추가

--- Loader/expressLoader.js
+++ Loader/expressLoader.js
@@ -4,12 +4,25 @@ const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
+
+const cspOptions = {
+  directives: {
+    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+    "script-src": ["'self'", "*.fontawesome.com", "*.naver.com", "*.pstatic.net"],
+    "connect-src": ["'self'", "*.datamoa.kr", "*.fontawesome.com", "*.naver.com", "*.navercorp.com", "*.kakao.com"],
+    "img-src": ["'self'", "data:", "*.pstatic.net", "*.navercorp.com"]
+  }
+};
+
 module.exports = async (app) => {
   // CORS
   app.use(cors());
 
   // Security header
-  app.use(helmet());
+  app.use(helmet({
+    contentSecurityPolicy: cspOptions,
+    crossOriginEmbedderPolicy: false
+  }));
 
   // Cookie parser
   app.use(cookieParser());
@@ -18,6 +31,7 @@ module.exports = async (app) => {
   app.use(express.json());
 
   // Static file
-  app.use(express.static(path.join(__dirname, '../API/Views/Build')));
-
+  app.use(express.static(path.join(__dirname, '../API/Views/build')));
 }
+
+module.exports
