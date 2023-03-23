20a79c63a82cbffba58ef89c27c2d75a32a6109d
ShinGyeongju <sinkj98@naver.com>
2023-03-23 15:08:52 +0900

    Express 라우트 순서 변경

--- API/Routes/router.js
+++ API/Routes/router.js
@@ -5,11 +5,11 @@ const logger = require('../../Common/logger');
 
 
 module.exports = (app) => {
-  // Datamoa page
-  app.use('/', logger.datamoaLogger.httpLogger, datamoaRouter);
-
   // Toilet page
   app.use('/toilet/', logger.toiletLogger.httpLogger, toiletRouter);
+
+  // Datamoa page
+  app.use('/', logger.datamoaLogger.httpLogger, datamoaRouter);
 
   // TODO: 복권방 위치 찾기 page 추가 예정.
   //
