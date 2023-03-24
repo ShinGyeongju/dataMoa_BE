fbbc2cd3af05167ae4c3ba2b12aba6714e7edcba
ShinGyeongju <sinkj98@naver.com>
2023-03-24 14:46:11 +0900

    화장실 데이터 수동 관리 관련 프로시저 및 트리거 추가

--- SQL/postgres_datamoa_init.sql
+++ SQL/postgres_datamoa_init.sql
@@ -1,5 +1,6 @@
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+SET TIME ZONE 'Asia/Seoul';
 
 ---------------------------------------------------------------
 -- Datamoa
@@ -63,6 +64,7 @@ CREATE DATABASE toilet;
 \connect toilet;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+SET TIME ZONE 'Asia/Seoul';
 
 CREATE TABLE tb_toilet_category (
     toilet_category_id INT GENERATED ALWAYS AS IDENTITY,
@@ -90,18 +92,59 @@ CREATE TABLE tb_toilet (
         ON UPDATE CASCADE
 );
 
+CREATE TABLE tb_toilet_manual (
+    toilet_id UUID DEFAULT uuid_generate_v4(),
+    toilet_category_id INT NOT NULL,
+    operation VARCHAR(10)  NOT NULL,
+    toilet_name VARCHAR(60)[] NOT NULL,
+    toilet_region VARCHAR(20) NOT NULL,
+    toilet_address VARCHAR(100),
+    toilet_road_address VARCHAR(100),
+    management_agency VARCHAR(40),
+    phone_number VARCHAR(20),
+    open_hour VARCHAR(60),
+    wsg84_x DOUBLE PRECISION NOT NULL,
+    wsg84_y DOUBLE PRECISION NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    CONSTRAINT pk_toilet_updated_id PRIMARY KEY (toilet_id),
+    CONSTRAINT fk_toilet_category_updated_id FOREIGN KEY (toilet_category_id)
+        REFERENCES "tb_toilet_category"(toilet_category_id)
+        ON UPDATE CASCADE
+);
+
 ---------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION func_update_time()
     RETURNS TRIGGER AS
         $$ BEGIN
-            NEW.updated_at = now();
-            RETURN NEW;
+            IF TG_OP = 'INSERT' THEN
+                INSERT
+                    INTO tb_toilet_manual (toilet_id, toilet_category_id, toilet_name, operation, toilet_region, toilet_address, toilet_road_address, management_agency, phone_number, open_hour, wsg84_x, wsg84_y)
+                    VALUES (NEW.toilet_id, NEW.toilet_category_id, NEW.toilet_name, 'insert', NEW.toilet_region, new.toilet_address, NEW.toilet_road_address, NEW.management_agency, NEW.phone_number, NEW.open_hour, NEW.wsg84_x, NEW.wsg84_y)
+                    ON CONFLICT (toilet_id) DO UPDATE
+                    SET toilet_category_id = NEW.toilet_category_id, toilet_name = NEW.toilet_name, operation = 'insert', toilet_region = NEW.toilet_region, toilet_address = NEW.toilet_address, toilet_road_address = NEW.toilet_road_address, management_agency = NEW.management_agency, phone_number = NEW.phone_number, open_hour = NEW.open_hour, wsg84_x = NEW.wsg84_x, wsg84_y = NEW.wsg84_y, updated_at = now();
+                RETURN NEW;
+            ELSIF TG_OP = 'UPDATE' THEN
+                NEW.updated_at = now();
+                INSERT
+                    INTO tb_toilet_manual (toilet_id, toilet_category_id, toilet_name, operation, toilet_region, toilet_address, toilet_road_address, management_agency, phone_number, open_hour, wsg84_x, wsg84_y)
+                    VALUES (NEW.toilet_id, NEW.toilet_category_id, NEW.toilet_name, 'update', NEW.toilet_region, new.toilet_address, NEW.toilet_road_address, NEW.management_agency, NEW.phone_number, NEW.open_hour, NEW.wsg84_x, NEW.wsg84_y)
+                    ON CONFLICT (toilet_id) DO UPDATE
+                    SET toilet_category_id = NEW.toilet_category_id, toilet_name = NEW.toilet_name, operation = 'update', toilet_region = NEW.toilet_region, toilet_address = NEW.toilet_address, toilet_road_address = NEW.toilet_road_address, management_agency = NEW.management_agency, phone_number = NEW.phone_number, open_hour = NEW.open_hour, wsg84_x = NEW.wsg84_x, wsg84_y = NEW.wsg84_y, updated_at = now();
+                RETURN NEW;
+            ELSIF TG_OP = 'DELETE' THEN
+                INSERT
+                    INTO tb_toilet_manual (toilet_id, toilet_category_id, toilet_name, operation, toilet_region, toilet_address, toilet_road_address, management_agency, phone_number, open_hour, wsg84_x, wsg84_y)
+                    VALUES (OLD.toilet_id, OLD.toilet_category_id, OLD.toilet_name, 'delete', OLD.toilet_region, OLD.toilet_address, OLD.toilet_road_address, OLD.management_agency, OLD.phone_number, OLD.open_hour, OLD.wsg84_x, OLD.wsg84_y)
+                    ON CONFLICT (toilet_id) DO UPDATE
+                    SET toilet_category_id = OLD.toilet_category_id, toilet_name = OLD.toilet_name, operation = 'delete', toilet_region = OLD.toilet_region, toilet_address = OLD.toilet_address, toilet_road_address = OLD.toilet_road_address, management_agency = OLD.management_agency, phone_number = OLD.phone_number, open_hour = OLD.open_hour, wsg84_x = OLD.wsg84_x, wsg84_y = OLD.wsg84_y, updated_at = now();
+                RETURN OLD;
+            END IF;
         END; $$
     LANGUAGE plpgsql;
 
-CREATE TRIGGER trig_update_time
-    BEFORE UPDATE ON tb_toilet
+CREATE OR REPLACE TRIGGER trig_update_time
+    BEFORE INSERT OR UPDATE OR DELETE ON tb_toilet
     FOR EACH ROW
     EXECUTE PROCEDURE func_update_time();
 
@@ -109,4 +152,3 @@ CREATE TRIGGER trig_update_time
 
 INSERT INTO tb_toilet_category (toilet_category_name)
     VALUES ('공중 화장실'), ('개방 화장실'), ('간이 화장실'), ('기타');
-
